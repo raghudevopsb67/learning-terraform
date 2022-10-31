73e88cbf0ebf41bedb457fec094447c6202cf7f0
Raghu K <raghuk.vit@gmail.com>
2022-10-31 07:09:04 +0530

    Update learning-terraform/

--- 02-variables/var.tf
+++ 02-variables/var.tf
@@ -65,3 +65,10 @@ variable "sample7" {}
 output "sample7" {
   value = var.sample7
 }
+
+## Pick a variable from SHELL ENV VARIABLE
+variable "sample8" {}
+output "sample8" {
+  value = var.sample8
+}
+
