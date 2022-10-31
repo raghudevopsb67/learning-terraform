c492953bc6b6bd574d6e0eed5c585c40d1a01157
Raghu K <raghuk.vit@gmail.com>
2022-10-31 06:54:07 +0530

    Update learning-terraform/

---
+++ 02-variables/var.tf
@@ -0,0 +1,13 @@
+variable "sample" {
+  default = "hello"
+}
+
+# Variable is not a combination then var.sample, if it is combining with any other string then ${var.sample}
+output "sample" {
+  value = var.sample
+}
+
+output "sample1" {
+  value = "Hey, ${var.sample}"
+}
+
