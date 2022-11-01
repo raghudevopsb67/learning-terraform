0c1552ad54eab8669ada402cf75ae7874e68238d
Raghu K <raghuk.vit@gmail.com>
2022-11-01 07:22:00 +0530

    Update learning-terraform/

--- 05-functions/main.tf
+++ 05-functions/main.tf
@@ -5,3 +5,20 @@ output "sample" {
 variable "sample" {
   default = ["abc", 123]
 }
+
+variable "sample1" {
+  default = {
+    orange = {
+      name  = "Orange"
+      count = 10
+    }
+    apple = {
+      name  = "Apple"
+      count = 100
+    }
+  }
+}
+
+output "sample1" {
+  value = lookup(var.sample1, "apple", null)
+}
