1ec9c522e6b751253bf926d50cc5dbe9225bdcca
Raghu K <raghuk.vit@gmail.com>
2022-11-02 06:36:10 +0530

    Update learning-terraform/

---
+++ 08-loops/03/main.tf
@@ -0,0 +1,25 @@
+//resource "aws_instance" "sample" {
+//  count         = length(var.instances)
+//  ami           = "ami-00d48a21603b2119b"
+//  instance_type = "t3.micro"
+//  tags = {
+//    Name = element(var.instances, count.index)
+//  }
+//}
+//
+
+variable "instances" {
+  default = {
+    catalogue = {
+      instance_type = "t3.micro"
+      tagName = "CATALOGUE"
+    }
+    cart = {
+      instance_type = "t3.micro"
+      tagName = "CART"
+    }
+
+  }
+}
+
+outp
