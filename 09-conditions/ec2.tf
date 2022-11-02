990604fe7e330b0020b863d02ae69022582ff0f7
Raghu K <raghuk.vit@gmail.com>
2022-11-02 07:00:52 +0530

    Update learning-terraform/

---
+++ 09-conditions/ec2.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "sample" {
+  ami                    = "ami-00d48a21603b2119b"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = aws_security_group.allow_ssh.*.id
+
+  tags = {
+    Name = "test"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  count       = var.create_sg ? 1 : 0
+  name        = "allow_ssh"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-075435ce9af088bfa"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+variable "create_sg" {
+  default = false
+}
